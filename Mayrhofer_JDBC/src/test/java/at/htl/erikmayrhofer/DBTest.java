aa108b9228730fac4490453a40412c712cf799ba
Erik Mayrhofer <obyoxar@gmail.com>
2018-10-22 20:45:53 +0200

    WIP on Course-Management

--- Mayrhofer_JDBC/src/test/java/at/htl/erikmayrhofer/DBTest.java
+++ Mayrhofer_JDBC/src/test/java/at/htl/erikmayrhofer/DBTest.java
@@ -6,10 +6,7 @@ import org.junit.runners.MethodSorters;
 
 import javax.xml.transform.Result;
 import java.sql.*;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.Period;
+import java.time.*;
 import java.time.temporal.TemporalAmount;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -46,21 +43,145 @@ public class DBTest {
                     "name VARCHAR(255) NOT NULL UNIQUE" +
                     ")";
             stmt.execute(crtSubjectString);
-            String crtAssignmentString = "CREATE TABLE assignment(" +
-                    "id INT CONSTRAINT assignment_pk PRIMARY KEY " +
+            String crtCourseString = "CREATE TABLE course(" +
+                    "id INT CONSTRAINT course_pk PRIMARY KEY " +
                     "GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1)," +
-                    "duedate DATE," +
-                    "description VARCHAR(2000)," +
+                    "time TIMESTAMP," +
+                    "description VARCHAR(1000)," +
                     "subject INT CONSTRAINT assignemnt_subject_fk REFERENCES subject(id)" +
                     ")";
-            stmt.execute(crtAssignmentString);
+            stmt.execute(crtCourseString);
+            String crtStudentString = "CREATE TABLE student(" +
+                    "id INT CONSTRAINT student_pk PRIMARY KEY " +
+                    "GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1)," +
+                    "name VARCHAR(30)" +
+                    ")";
+            stmt.execute(crtStudentString);
+            String crtRegistrationtring = "CREATE TABLE registration(" +
+                    "id INT CONSTRAINT registration_pk PRIMARY KEY " +
+                    "GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1)," +
+                    "course INT CONSTRAINT registration_course_fk REFERENCES course(id)," +
+                    "student INT CONSTRAINT registration_student_fk REFERENCES student(id)" +
+                    ")";
+            stmt.execute(crtRegistrationtring);
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     @Test
-    public void t01_dml(){
+    public void t01_subjectmeta() {
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet set = stmt.executeQuery(
+                    "SELECT columnname FROM " +
+                    "sys.SYSCOLUMNS c LEFT JOIN sys.SYSTABLES t ON(c.REFERENCEID = t.TABLEID)" +
+                            "WHERE UPPER(t.TABLENAME) = 'SUBJECT' ORDER BY c.COLUMNNAME"
+            );
+            set.next();
+            assertThat(set.getString(1), is("ID"));
+            set.next();
+            assertThat(set.getString(1), is("NAME"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void t02_coursemeta() {
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet set = stmt.executeQuery(
+                    "SELECT columnname FROM " +
+                            "sys.SYSCOLUMNS c LEFT JOIN sys.SYSTABLES t ON(c.REFERENCEID = t.TABLEID)" +
+                            "WHERE UPPER(t.TABLENAME) = 'COURSE' ORDER BY c.COLUMNNAME"
+            );
+            set.next();
+            assertThat(set.getString(1), is("DESCRIPTION"));
+            set.next();
+            assertThat(set.getString(1), is("ID"));
+            set.next();
+            assertThat(set.getString(1), is("SUBJECT"));
+            set.next();
+            assertThat(set.getString(1), is("TIME"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void t03_studentmeta() {
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet set = stmt.executeQuery(
+                    "SELECT columnname FROM " +
+                            "sys.SYSCOLUMNS c LEFT JOIN sys.SYSTABLES t ON(c.REFERENCEID = t.TABLEID)" +
+                            "WHERE UPPER(t.TABLENAME) = 'STUDENT' ORDER BY c.COLUMNNAME"
+            );
+            set.next();
+            assertThat(set.getString(1), is("ID"));
+            set.next();
+            assertThat(set.getString(1), is("NAME"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void t04_registrationmeta() {
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet set = stmt.executeQuery(
+                    "SELECT columnname FROM " +
+                            "sys.SYSCOLUMNS c LEFT JOIN sys.SYSTABLES t ON(c.REFERENCEID = t.TABLEID)" +
+                            "WHERE UPPER(t.TABLENAME) = 'REGISTRATION' ORDER BY c.COLUMNNAME"
+            );
+            set.next();
+            assertThat(set.getString(1), is("COURSE"));
+            set.next();
+            assertThat(set.getString(1), is("ID"));
+            set.next();
+            assertThat(set.getString(1), is("STUDENT"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void t05_student(){
+        int countInserts = 0;
+        try{
+            String insertString = "INSERT INTO student(name) VALUES(?)";
+            PreparedStatement subsmt = conn.prepareStatement(insertString);
+            subsmt.setString(1, "Erik Mayrhofer");
+            countInserts += subsmt.executeUpdate();
+            subsmt.setString(1, "Jan Neuburger");
+            countInserts += subsmt.executeUpdate();
+            subsmt.setString(1, "Florian Schwarcz");
+            countInserts += subsmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        assertThat(countInserts, is(3));
+
+        try {
+            PreparedStatement psmt = conn.prepareStatement("SELECT name FROM subject");
+            ResultSet rs = psmt.executeQuery();
+            rs.next();
+            assertThat(rs.getString(1), is("Erik Mayrhofer"));
+            rs.next();
+            assertThat(rs.getString(1), is("Jan Neuburger"));
+            rs.next();
+            assertThat(rs.getString(1), is("Florian Schwarcz"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void t06_subject(){
         int countInserts = 0;
         try{
             String insertSubString = "INSERT INTO subject(name) VALUES(?)";
@@ -91,28 +212,43 @@ public class DBTest {
     }
 
     @Test
-    public void t02_assignment(){
+    public void t07_course(){
         int countInserts = 0;
         try{
-            String insertAssString = "INSERT INTO assignment(duedate, description, subject)" +
-                    "VALUES(?, ?, (SELECT id FROM subject WHERE name = ?))";
-            PreparedStatement psmt = conn.prepareStatement(insertAssString);
-            psmt.setDate(1, java.sql.Date.valueOf(LocalDate.now().plus(Period.ofDays(2))));
-            psmt.setString(2, "Testassignment");
-            psmt.setString(3, "Deutsch");
-            countInserts += psmt.executeUpdate();
+            String insertSubString = "INSERT INTO course(time, description, subject) " +
+                    "VALUES(?, ?, (SELECT id FROM SUBJECT WHERE NAME = ?))";
+            PreparedStatement subsmt = conn.prepareStatement(insertSubString);
+            subsmt.setTimestamp(1, Timestamp.from(Instant.now()));
+            subsmt.setString(2,"Simple Algebra");
+            subsmt.setString(3, "Mathematik");
+            countInserts += subsmt.executeUpdate();
+            subsmt.setTimestamp(1, Timestamp.from(Instant.now()));
+            subsmt.setString(2,"Passive Voice");
+            subsmt.setString(3, "Englisch");
+            countInserts += subsmt.executeUpdate();
+            subsmt.setTimestamp(1, Timestamp.from(Instant.now()));
+            subsmt.setString(2,"Konjugation");
+            subsmt.setString(3, "Deutsch");
+            countInserts += subsmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        assertThat(countInserts, is(1));
+        assertThat(countInserts, is(3));
 
-        try{
-            String getAssString = "SELECT a.duedate, a.description, s.name FROM assignment a " +
-                    "LEFT JOIN subject s ON(a.subject = s.id)";
-            PreparedStatement psmt = conn.prepareStatement(getAssString);
+        try {
+            PreparedStatement psmt = conn.prepareStatement(
+                    "SELECT c.time, c.description, s.name FROM course c " +
+                            "LEFT JOIN SUBJECT s ON (c.subject = s.id)"
+            );
             ResultSet rs = psmt.executeQuery();
             rs.next();
-            assertThat(rs.getString(2), is("Testassignment"));
+            assertThat(rs.getString(2), is("Simple Algebra"));
+            assertThat(rs.getString(3), is("Mathematik"));
+            rs.next();
+            assertThat(rs.getString(2), is("Passive Voice"));
+            assertThat(rs.getString(3), is("Englisch"));
+            rs.next();
+            assertThat(rs.getString(2), is("Konjugation"));
             assertThat(rs.getString(3), is("Deutsch"));
         } catch (SQLException e) {
             e.printStackTrace();
@@ -124,10 +260,10 @@ public class DBTest {
         //Drop tables
         try{
             Statement stmt = conn.createStatement();
-            String dropAssignmentString = "DROP TABLE assignment";
-            stmt.execute(dropAssignmentString);
-            String dropSubjectString = "DROP TABLE subject";
-            stmt.execute(dropSubjectString);
+            stmt.execute("DROP TABLE registration");
+            stmt.execute("DROP TABLE course");
+            stmt.execute("DROP TABLE subject");
+            stmt.execute("DROP TABLE student");
         } catch (SQLException e) {
             e.printStackTrace();
         }
